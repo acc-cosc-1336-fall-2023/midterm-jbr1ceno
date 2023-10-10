befe81b0e74a8e81230163714902c96c946dac89
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-10-10 18:11:52 +0000

    Question 1 A WIP

--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,13 @@
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+
+def reverse_string(string1):
+
+    string_length = len(string1) - 1
+
+    while(string_length >= 0):
+        #print(str(string1[string_length]))
+        string_length -= 1
+        return string_length
+    
